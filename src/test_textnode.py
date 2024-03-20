1f5182a012c3dfe6f538571f7311f52d52d70cec
Levi Powis <lpowis97@gmail.com>
2024-03-20 16:23:26 -0400

    Add test_eq_url to test_textnode.py

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -26,6 +26,11 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node2", text_type_text)
         self.assertNotEqual(node, node2)
 
+    def test_eq_url(self):
+        node = TextNode("This is a text node", text_type_italic, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", text_type_italic, "https://www.boot.dev")
+        self.assertEqual(node, node2)
+
 
 if __name__ == "__main__":
     unittest.main()
